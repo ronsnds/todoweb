07ef8993d9a1f7f0cf064aa8cf1f233be01cabf3
Ronaldo-max <ronaldodcs21@gmail.com>
2022-04-07 21:26:36 -0300

    organização

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import "./styles/reset.css";
+import Home from "./pages/home";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 reportWebVitals();
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { AddArea } from "./components/addArea";
+import "./style.css";
+import { ListAdd } from "./components/listAdd";
+
+function Home() {
+  const [itemtask, setItem] = useState([]);
+
+  function addTask(newTask) {
+    let newList = [...itemtask];
+    newList.push({
+      id: itemtask.length + 1,
+      task: newTask,
+      done: false,
+    });
+    setItem(newList);
+  }
+
+  function handleTaskChange(id, done) {
+    let newList = [...itemtask];
+    for (let i in newList) {
+      if (newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setItem(newList);
+  }
+
+  return (
+    <div className="home">
+      <h1>TodoWeb</h1>
+      <AddArea onEnter={addTask} />
+      {itemtask.map((todo, index) => (
+        <ListAdd key={index} item={todo} onChange={handleTaskChange} />
+      ))}
+    </div>
+  );
+}
+
+export default Home;
